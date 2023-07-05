7236f8f54625fb754a6bf100c16507f915fd9d80
nitikasuresh <nitikasuresh@gmail.com>
2023-07-05 14:24:25 -0400

    modified multicam_loop script

--- 2D_vision/scripts/multicam_loop.py
+++ 2D_vision/scripts/multicam_loop.py
@@ -60,12 +60,17 @@ config_5.enable_stream(rs.stream.depth, W, H, rs.format.z16, 30)
 config_5.enable_stream(rs.stream.color, W, H, rs.format.bgr8, 30)
 
 # start streaming
-pipeline_1.start(config_1)
-pipeline_2.start(config_2)
-pipeline_3.start(config_3)
-pipeline_4.start(config_4)
-pipeline_5.start(config_5)
+p1 = pipeline_1.start(config_1)
+p2 = pipeline_2.start(config_2)
+p3 = pipeline_3.start(config_3)
+p4 = pipeline_4.start(config_4)
+p5 = pipeline_5.start(config_5)
 
+ds1 = p1.get_device().first_depth_sensor().get_depth_scale()
+ds2 = p2.get_device().first_depth_sensor().get_depth_scale()
+ds3 = p3.get_device().first_depth_sensor().get_depth_scale()
+ds4 = p4.get_device().first_depth_sensor().get_depth_scale()
+ds5 = p5.get_device().first_depth_sensor().get_depth_scale()
 # align stream
 aligned_stream_1 = rs.align(rs.stream.color)
 aligned_stream_2 = rs.align(rs.stream.color)
@@ -73,13 +78,17 @@ aligned_stream_3 = rs.align(rs.stream.color)
 aligned_stream_4 = rs.align(rs.stream.color)
 aligned_stream_5 = rs.align(rs.stream.color)
 
-for i in range(650):
+# for i in range(650):
+i = 0
+while(True):
 	# Camera 1
 	frames_1 = pipeline_1.wait_for_frames()
 	frames_1 = aligned_stream_1.process(frames_1)
 	color_frame_1 = frames_1.get_color_frame()
 	depth_frame_1 = frames_1.get_depth_frame().as_depth_frame()
 	color_image_1 = np.asanyarray(color_frame_1.get_data())
+	depth_image_1 = np.asanyarray(depth_frame_1.get_data())
+	depth_image_1 = depth_image_1/(depth_image_1.max()/255.0)
 
 	# Camera 2
 	frames_2 = pipeline_2.wait_for_frames()
@@ -87,6 +96,8 @@ for i in range(650):
 	color_frame_2 = frames_2.get_color_frame()
 	depth_frame_2 = frames_2.get_depth_frame().as_depth_frame()
 	color_image_2 = np.asanyarray(color_frame_2.get_data())
+	depth_image_2 = np.asanyarray(depth_frame_2.get_data())
+	depth_image_2 = depth_image_2/(depth_image_2.max()/255.0)
 
 	# Camera 3
 	frames_3 = pipeline_3.wait_for_frames()
@@ -94,6 +105,8 @@ for i in range(650):
 	color_frame_3 = frames_3.get_color_frame()
 	depth_frame_3 = frames_3.get_depth_frame().as_depth_frame()
 	color_image_3 = np.asanyarray(color_frame_3.get_data())
+	depth_image_3 = np.asanyarray(depth_frame_3.get_data())
+	depth_image_3 = depth_image_3/(depth_image_3.max()/255.0)
 
 	# Camera 2
 	frames_4 = pipeline_4.wait_for_frames()
@@ -101,6 +114,8 @@ for i in range(650):
 	color_frame_4 = frames_4.get_color_frame()
 	depth_frame_4 = frames_4.get_depth_frame().as_depth_frame()
 	color_image_4 = np.asanyarray(color_frame_4.get_data())
+	depth_image_4 = np.asanyarray(depth_frame_4.get_data())
+	depth_image_4 = depth_image_4/(depth_image_4.max()/255.0)
 
 	# Camera 2
 	frames_5 = pipeline_5.wait_for_frames()
@@ -108,6 +123,8 @@ for i in range(650):
 	color_frame_5 = frames_5.get_color_frame()
 	depth_frame_5 = frames_5.get_depth_frame().as_depth_frame()
 	color_image_5 = np.asanyarray(color_frame_5.get_data())
+	depth_image_5 = np.asanyarray(depth_frame_5.get_data())
+	depth_image_5 = depth_image_5/(depth_image_5.max()/255.0)
 
 	# Show the images
 	cv2.imshow("Camera 1", color_image_1)
@@ -115,22 +132,41 @@ for i in range(650):
 	cv2.imshow("Camera 3", color_image_3)
 	cv2.imshow("Camera 4", color_image_4)
 	cv2.imshow("Camera 5", color_image_5)
-	cv2.waitKey(50)
-cv2.destroyAllWindows()
-	# if k==27:
-	# 	cv2.destroyAllWindows()
-	# elif k==ord('s'):
-	# 	print("Saving....")
-	# 	# Save the images
-	# 	# cam1_filename = "scripts/Calib_Imgs/" + i + "_cam1.jpg"
-	# 	cam2_filename = "scripts/Calib_Imgs/" + str(i) + "_cam2.jpg"
-	# 	cam3_filename = "scripts/Calib_Imgs/" + str(i) + "_cam3.jpg"
-	# 	cam4_filename = "scripts/Calib_Imgs/" + str(i) + "_cam4.jpg"
-	# 	cam5_filename = "scripts/Calib_Imgs/" + str(i) + "_cam5.jpg"
+	k = cv2.waitKey(1)
+# cv2.destroyAllWindows()
+	if k==27:
+		break
+	elif k==ord('s'):
+		print("Saving....")
+		print(depth_frame_1.get_distance(430,175))
+		# Save the images
+		cam1_filename = "/home/franka/frankapy/scripts/socket_Imgs/color/" + str(i) + "_cam1.jpg"
+		cam2_filename = "/home/franka/frankapy/scripts/socket_Imgs/color/" + str(i) + "_cam2.jpg"
+		cam3_filename = "/home/franka/frankapy/scripts/socket_Imgs/color/" + str(i) + "_cam3.jpg"
+		cam4_filename = "/home/franka/frankapy/scripts/socket_Imgs/color/" + str(i) + "_cam4.jpg"
+		cam5_filename = "/home/franka/frankapy/scripts/socket_Imgs/color/" + str(i) + "_cam5.jpg"
 
-	# 	# cv2.imwrite(cam1_filename, color_image_1)
-	# 	cv2.imwrite(cam2_filename, color_image_2)
-	# 	cv2.imwrite(cam3_filename, color_image_3)
-	# 	cv2.imwrite(cam4_filename, color_image_4)
-	# 	cv2.imwrite(cam5_filename, color_image_5)
+		cam1_filename_d = "/home/franka/frankapy/scripts/socket_Imgs/depth/" + str(i) + "_cam1.jpg"
+		cam2_filename_d = "/home/franka/frankapy/scripts/socket_Imgs/depth/" + str(i) + "_cam2.jpg"
+		cam3_filename_d = "/home/franka/frankapy/scripts/socket_Imgs/depth/" + str(i) + "_cam3.jpg"
+		cam4_filename_d = "/home/franka/frankapy/scripts/socket_Imgs/depth/" + str(i) + "_cam4.jpg"
+		cam5_filename_d = "/home/franka/frankapy/scripts/socket_Imgs/depth/" + str(i) + "_cam5.jpg"
+		if i == 0:
+			print("i = 0")
+			cv2.imwrite(cam2_filename, color_image_2)
+			cv2.imwrite(cam3_filename, color_image_3)
+			cv2.imwrite(cam4_filename, color_image_4)
+			cv2.imwrite(cam5_filename, color_image_5)
+
+			cv2.imwrite(cam2_filename_d, depth_image_2)
+			cv2.imwrite(cam3_filename_d, depth_image_3)
+			cv2.imwrite(cam4_filename_d, depth_image_4)
+			cv2.imwrite(cam5_filename_d, depth_image_5)
+
+		cv2.imwrite(cam1_filename, color_image_1)
+		cv2.imwrite(cam1_filename_d, depth_image_1)
+		# print(np.unique(depth_image_1))
+		
+		i += 1
+cv2.destroyAllWindows()
 		
